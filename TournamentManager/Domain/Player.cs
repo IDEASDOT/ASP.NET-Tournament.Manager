fbf5e7578139331edece341124dc3defd81f4ad9
Egert Aia <aiaegert@gmail.com>
2016-02-13 09:48:08 +0200

    Added the mainframe of the project. Started creating the DataBaseFiles.
    TODO: Finish the Database

---
+++ TournamentManager/Domain/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Player
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string NickName { get; set; }
+        public string LastName { get; set; }
+
+        #region NotMapped
+
+        public string FullName => FirstName + " '" + NickName + "' " + LastName;
+        public string FirstLastName => FirstName + " " + LastName;
+        public string LastFirstName => LastName + " " + FirstName;
+
+        #endregion
+    }
+}
